afe22e4834436ba9b9318dd3df7976676cb9fc83
Wille Faler <wille.faler@gmail.com>
2021-08-15 13:20:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,27 @@
-#curl -L https://nixos.org/nix/install | sh
-#. $HOME/.nix-profile/etc/profile.d/nix.sh
-#nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-#nix-channel --update
-#nix-shell '<home-manager>' -A install
+# Add Neovim PPA
+sudo add-apt-repository -y ppa:neovim-ppa/unstable 
+# Add dependencies of terraform & gcloud
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl apt-transport-https ca-certificates
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 
-#curl -L https://github.com/wfaler/dotfiles/blob/main/home.nix --output $HOME/.config/nixpkgs/home.nix
-#home-manager switch
+# add gcloud repos
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# install terraform, neovim, ripgrep, gcloud, cloc and tmux
+sudo apt-get update && sudo apt-get install -y terraform neovim ripgrep google-cloud-sdk cloc tmux
+
+
+# TODO
+# pkgs.kubeval
+# pkgs.kubectx
+# pkgs.kubernetes-helm
+# pkgs.kustomize 
+# pkgs.direnv
+# pkgs.packer
+# pkgs.git-secret
+# pkgs.awscli
+# pkgs.protoc-gen-go
+# tmux settings
+# nvim settings
