0bf7fdbd5e91706212a5343977e2826e7b98933f
Wille Faler <wille.faler@gmail.com>
2021-08-15 11:53:25 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-curl -L https://nixos.org/nix/install | sh
-. $HOME/.nix-profile/etc/profile.d/nix.sh
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-nix-shell '<home-manager>' -A install
+#curl -L https://nixos.org/nix/install | sh
+#. $HOME/.nix-profile/etc/profile.d/nix.sh
+#nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+#nix-channel --update
+#nix-shell '<home-manager>' -A install
 
-curl -L https://github.com/wfaler/dotfiles/blob/main/home.nix --output $HOME/.config/nixpkgs/home.nix
-home-manager switch
+#curl -L https://github.com/wfaler/dotfiles/blob/main/home.nix --output $HOME/.config/nixpkgs/home.nix
+#home-manager switch
