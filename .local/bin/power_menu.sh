6bea364c0aeea0fea5ecf5d203560db0e4853df9
Wille Faler <wfaler@chaordic.io>
2024-08-24 22:51:42 +0200

    turn off screen on screen lock

--- .local/bin/power_menu.sh
+++ .local/bin/power_menu.sh
@@ -4,12 +4,13 @@
 options=("Lock screen" "Logout" "Reboot" "Shutdown")
 
 # Show the options in rofi and get the selected one
-selected=$(printf '%s\n' "${options[@]}" | rofi -dmenu -i -p "System operation:")
+selected=$(printf '%s\n' "${options[@]}" | rofi -dmenu -i -p "Power menu")
 
 # Perform the selected action
 case "$selected" in
     "Lock screen")
-        i3lock -c 000000  # You can customize this command
+        i3lock && xset dpms force off
+       # i3lock -c 000000  # You can customize this command
         ;;
     "Logout")
         i3-msg exit
