600a259bfbb8aafab07aa8102d53081550f9f95d
Wille Faler <wfaler@chaordic.io>
2024-10-21 20:37:53 +0200

    fish in kitty

---
+++ README.md
@@ -0,0 +1,41 @@
+Run:
+```
+install.sh
+```
+Restart, then run:
+```
+post_install.sh
+```
+
+Enter `fish`, then:
+```
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install 0rax/fishline
+```
+
+
+Client apps to install via app centre:
+```
+Spotify
+Discord
+Slack
+Libreoffice
+Todoist
+Darktable
+Whatsie
+OBS Studio
+Bruno
+Signal
+Zoom
+Obsidian
+Audacity
+Flameshot
+```
+
+Other apps:
+```
+Zoom
+Calibre
+Evolution
+```
+
--- post_install.sh
+++ post_install.sh
@@ -1,37 +1,27 @@
 #!/bin/bash
-# Function to check if a command exists
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-# Check if Nix is installed
 if ! command_exists nix; then
     echo "Nix is not installed. Please install Nix first."
     exit 1
 fi
 
-# Check if home-manager is already installed
 if ! command_exists home-manager; then
    nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 
-# Update channels
    nix-channel --update
 
-# Install home-manager
    nix-shell '<home-manager>' -A install
 
-# Export NIXPKGS_ALLOW_UNFREE
    export NIXPKGS_ALLOW_UNFREE=1
 
-# Run home-manager switch
    home-manager switch
-
 
 fi
 
-# If we've reached this point, Nix is installed but home-manager is not
 
-# Add home-manager channel
 if ! grep -q "$(which fish)" /etc/shells; then
     echo "Adding fish to /etc/shells..."
     echo "$(which fish)" | sudo tee -a /etc/shells
@@ -39,7 +29,6 @@ else
     echo "Fish is already in /etc/shells."
 fi
 
-# Change user's default shell to fish if it's not already
 if [ "$SHELL" != "$(which fish)" ]; then
     echo "Changing default shell to fish..."
     chsh -s "$(which fish)"
@@ -47,7 +36,6 @@ else
     echo "Fish is already the default shell."
 fi
 
-
-
+mise install
 
 echo "home-manager has been installed and configured successfully."
